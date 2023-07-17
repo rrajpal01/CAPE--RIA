0952630ae3e8f04edfda27cd5545b9f3b0d6d8a3
Ria Rajpal <riarajpal@Rias-MacBook-Air.local>
2023-07-17 18:18:35 -0400

    full react project

--- ria-cape-react/src/App.js
+++ ria-cape-react/src/App.js
@@ -3,33 +3,59 @@ import ReactDOM from 'react-dom';
 
 
 
-const person = {
-  name: "ria",
-  email: "riarajpal13@",
-  age: 0
-};
+
+const person = [
+  {
+    name: "John",
+    email: "john@example.com",
+    age: 20
+  },
+  {
+    name: "Jane",
+    email: "jane@example.com",
+    age: 30
+  },
+  {
+    name: "Jack",
+    email: "jack@example.com",
+    age: 40
+  }
+ ]
  
-const MyComponent =
-<UserProfile
-name = {person.name}
-email = {person.email}
-age = {person.age}
-/>
-function UserProfile({name, email, age})
-{
-return(
-  <div>
-    <p> Name: {name}</p>
-    <p> Email: {email}</p>
-    <p> Age: {age}</p>
-  </div>
-)
-}
+ 
+// const MyComponent =
+// <UserProfile
+// name = {person.name}
+// email = {person.email}
+// age = {person.age}
+// />
+// function UserProfile({name, email, age})
+// {
+// return(
+//   <div>
+//     <p> Name: {name}</p>
+//     <p> Email: {email}</p>
+//     <p> Age: {age}</p>
+//   </div>
+// )
+// }
 function App() {
   
-  return <UserProfile name={person.name} email={person.email} age={person.age} /> ;
+  return (
+    <>
+    {person.map(function(person) {
+      return (
+        <div key={person.id}>
+          <p> Name: {person.name}</p>
+          <p> Email: {person.email}</p>
+          <p>Age : {person.hackathons}</p>
 
+        </div>
+      )
+    })}
+    </>
 
+  )
 }
 
 export default App;
